2e08dd320d17581552210e175a59bc0e6e5e07ec
David M <62346025+dispherical@users.noreply.github.com>
2025-02-18 02:02:25 -0500

    feat: prettier, add backlogging

--- public/utils.js
+++ public/utils.js
@@ -1,15 +1,23 @@
-window.skewLocation = function skewLocation(lat, lon, minDistanceKm = 2, maxDistanceKm = 2.7) {
-    const earthRadiusKm = 6371; 
+window.skewLocation = function skewLocation(
+  lat,
+  lon,
+  minDistanceKm = 2,
+  maxDistanceKm = 2.7,
+) {
+  const earthRadiusKm = 6371;
 
-    const minDistRad = minDistanceKm / earthRadiusKm;
-    const maxDistRad = maxDistanceKm / earthRadiusKm;
+  const minDistRad = minDistanceKm / earthRadiusKm;
+  const maxDistRad = maxDistanceKm / earthRadiusKm;
 
-    const randomDist = minDistRad + Math.random() * (maxDistRad - minDistRad);
-    const randomAngle = Math.random() * 2 * Math.PI;
+  const randomDist = minDistRad + Math.random() * (maxDistRad - minDistRad);
+  const randomAngle = Math.random() * 2 * Math.PI;
 
-    const newLat = lat + (randomDist * Math.cos(randomAngle)) * (180 / Math.PI);
+  const newLat = lat + randomDist * Math.cos(randomAngle) * (180 / Math.PI);
 
-    const newLon = lon + (randomDist * Math.sin(randomAngle)) * (180 / Math.PI) / Math.cos(lat * Math.PI / 180);
+  const newLon =
+    lon +
+    (randomDist * Math.sin(randomAngle) * (180 / Math.PI)) /
+      Math.cos((lat * Math.PI) / 180);
 
-    return { latitude: newLat, longitude: newLon };
-}
+  return { latitude: newLat, longitude: newLon };
+};
--- schemas.js
+++ schemas.js
@@ -1,11 +1,21 @@
-const { z } = require("zod")
+import { z } from "zod";
 
-module.exports = {
-    newPost: z.object({
-        latitude: z.union([z.string(), z.number()]),
-        longitude: z.union([z.string(), z.number()]),
-        text: z.string(),
-        date: z.string(),
-        parent: z.string().optional(),
-    })
-}
+export const newPost = z.object({
+  latitude: z.union([z.string(), z.number()]),
+  longitude: z.union([z.string(), z.number()]),
+  text: z.string(),
+  date: z.string(),
+  parent: z.string().optional(),
+});
+
+export const catchUp = z
+  .object({
+    max: z.number().min(1).optional().default(100),
+  })
+  .partial()
+  .default({});
+
+export default {
+  newPost,
+  catchUp,
+};
